dbb4a00d7aaea1ec878040ccd0ffbe510a9296b3
llopFilms <llopfilms@gmail.com>
2023-10-04 20:49:24 +0200

    Actualització

--- src/lib/data/contingutWeb.ts
+++ src/lib/data/contingutWeb.ts
@@ -1,12 +1,6 @@
 import fingerprint from '../../assets/svg/undraw_fingerprint_login_re_t71l.svg';
 import logoAstro from '../../assets/svg/favicon.svg';
 
-export type capssaleraT = {
-  id: number;
-  imatge: string;
-  alt: string;
-};
-
 export const capssalera = [
   {
     id: 1,
@@ -14,12 +8,6 @@ export const capssalera = [
     alt: 'fingerprint',
   },
 ];
-
-export type mesInfoT = {
-  id: number;
-  text: string;
-  imatge: string;
-};
 
 export const mesInfo = [
   {
@@ -44,12 +32,6 @@ export const mesInfo = [
   },
 ];
 
-export type carateristiquesT = {
-  id: number;
-  titol: string;
-  text: string;
-};
-
 export const caracteristiques = [
   {
     id: 1,
@@ -73,13 +55,44 @@ export const caracteristiques = [
   },
   {
     id: 5,
-    titol:
-      'Lorem ipsum dolor, sit amet',
+    titol: 'Lorem ipsum dolor, sit amet',
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, animi iste eius assumenda eum necessitatibus optio ad excepturi, ut dolorum eveniet quas reiciendis blanditiis?',
   },
   {
     id: 6,
     titol: 'Lorem ipsum dolor',
-    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum eum, provident expedita, porro quod officiis earum repellendus voluptatum illo tempore asperiores. Voluptatem odit, beatae unde sunt hic assumenda aspernatur commodi nesciunt. Id, eaque.',
+    text: 'Lorem ipsum dolor sit amet repellendus, adipisicing elit. Harum eum, provident expedita, porro quod officiis earum repellendus voluptatum illo tempore asperiores. Voluptatem odit, beatae unde sunt hic assumenda aspernatur commodi nesciunt. Id, eaque.',
+    claus: ['asperiores', 'repellendus', 'repellendus'],
+  },
+];
+
+export const projectes = [
+  {
+    id: 1,
+    titol: 'Lorem ipsum dolor',
+    descripcio:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed doloribus ex eius exercitationem, ut dolorem quidem pariatur eveniet autem laborum ipsa inventore. Libero tenetur est dolore dicta, recusandae saepe enim repellat aliquam vel odio, quia non officia commodi aperiam repudiandae veritatis voluptates impedit ad architecto! Vero vitae doloribus ad nostrum neque dolores non iusto, aliquam vel in, fugit tempora minus. Consequuntur, numquam officiis accusamus sit architecto totam molestiae fugit hic amet enim iusto velit iure eum? Voluptatum deserunt corporis, animi magnam nam, eius nobis nemo vero, quaerat officia hic.',
+    claus: ['voluptates', 'repudiandae', 'veritatis'],
+  },
+  {
+    id: 2,
+    titol: 'Lorem ipsum',
+    descripcio:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed doloribus ex eius exercitationem, ut dolorem quidem pariatur eveniet autem laborum ipsa inventore. Libero tenetur est dolore dicta, recusandae saepe enim repellat aliquam vel odio, quia non officia commodi aperiam repudiandae veritatis voluptates impedit ad architecto! Vero vitae doloribus ad nostrum neque dolores non iusto, aliquam vel in, fugit tempora minus. Consequuntur, numquam officiis accusamus sit architecto totam molestiae fugit hic amet enim iusto velit iure eum? Voluptatum deserunt corporis, animi magnam nam, eius nobis nemo vero, quaerat officia hic. Commodi nobis unde sapiente! Illum voluptate id accusamus amet dolorem dolores deserunt aliquam magnam voluptates? Culpa mollitia labore et? Beatae, commodi.',
+    claus: ['architecto', 'accusamus', 'commodi'],
+  },
+  {
+    id: 3,
+    titol: 'Lorem ipsum dolor sit amet consectetur.',
+    descripcio:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed doloribus ex eius exercitationem, ut dolorem quidem pariatur eveniet autem laborum ipsa inventore. Libero tenetur est dolore dicta, recusandae saepe enim repellat aliquam vel odio, quia non officia commodi aperiam repudiandae veritatis voluptates impedit ad architecto! Vero vitae doloribus ad nostrum neque dolores non iusto, aliquam vel in, fugit tempora minus. Consequuntur, numquam officiis accusamus sit architecto totam molestiae fugit hic amet enim iusto velit iure eum? Voluptatum deserunt corporis, animi magnam nam, eius nobis nemo vero, quaerat officia hic.',
+    claus: ['inventore', 'exercitationem', 'velit'],
+  },
+  {
+    id: 4,
+    titol: 'Lorem ipsum dolor sit amet  consectetur adipisicing elit.',
+    descripcio:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed doloribus ex eius exercitationem, ut dolorem quidem pariatur eveniet autem laborum ipsa inventore. Libero tenetur est dolore dicta, recusandae saepe enim repellat aliquam vel odio, quia non officia commodi aperiam repudiandae veritatis voluptates impedit ad architecto! Vero vitae doloribus ad nostrum neque dolores non iusto, aliquam vel in, fugit tempora minus.',
+    claus: ['exercitationem', 'iusto', 'fugit'],
   },
 ];
---
+++ src/lib/types/contingutTypes.ts
@@ -0,0 +1,24 @@
+export type capssaleraT = {
+  id: number;
+  imatge: string;
+  alt: string;
+};
+
+export type mesInfoT = {
+  id: number;
+  text: string;
+  imatge: string;
+};
+
+export type carateristiquesT = {
+  id: number;
+  titol: string;
+  text: string;
+};
+
+export type projectesT = {
+	id: number;
+	titol: string;
+  descripcio: string;
+  claus: string[];
+}
