b60c95fe6e50a45b5f51ad9633950c0797a402ad
llopFilms <llopfilms@gmail.com>
2023-10-07 23:25:38 +0200

    Actualització

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://llopfilms.github.io',
+  site: 'https://espai-astro.github.io',
   base: '/astro-slp',
 });
